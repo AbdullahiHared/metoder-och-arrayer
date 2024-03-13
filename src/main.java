f051512f6a5a2f2cc80238980aab77fd9851d3b1
Abdullahi <muxsinxareed6@gmail.com>
2024-03-13 10:48:10 +0100

    Skriva ut innehållet i en array med hjälp av metod

--- src/main.java
+++ src/main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class main {
+import java.util.Arrays;
+
+public class main {
+    static void skriv_ut_array (int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
+    public static void main(String[] args) {
+       int[] minArray = {1,2,3,4,5,6,8,9,10,11};
+
+        for (int i = 0; i < minArray.length; i++) {
+            System.out.println(minArray[i]);
+        }
+
+        skriv_ut_array(minArray);
+
+    }
 }
