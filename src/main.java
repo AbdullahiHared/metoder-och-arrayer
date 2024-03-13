dce14550090048483c703bf6f680762f98c3a5d5
Abdullahi <muxsinxareed6@gmail.com>
2024-03-13 10:57:57 +0100

    Skriva ut antal tal över 8

--- src/main.java
+++ src/main.java
@@ -3,15 +3,20 @@ import java.util.Arrays;
 public class main {
     static void skriv_ut_array (int[] array) {
         System.out.println(Arrays.toString(array));
+
+    }
+
+    static void antal_över_8(int[] array) {
+        System.out.println("Tal som är över 8 i arrayen");
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > 8) {
+                System.out.println(array[i]);
+            }
+        }
     }
     public static void main(String[] args) {
        int[] minArray = {1,2,3,4,5,6,8,9,10,11};
-
-        for (int i = 0; i < minArray.length; i++) {
-            System.out.println(minArray[i]);
-        }
-
         skriv_ut_array(minArray);
-
+        antal_över_8(minArray);
     }
 }
