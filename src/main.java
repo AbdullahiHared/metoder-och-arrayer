600bb92bcccc37c50be38535f10cc3310f7ecd13
Abdullahi <muxsinxareed6@gmail.com>
2024-03-16 16:44:06 +0100

    swap position for elements in an array

--- src/main.java
+++ src/main.java
@@ -14,9 +14,26 @@ public class main {
             }
         }
     }
+
+
+    static int hitta_tal(int[] array, int tal) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == tal) {
+                return i; // Return index if number is found
+            }
+        }
+        return -1; // Return -1 if number is not found
+    }
+
+    static void bytPlats (int array[], int element1, int element2 ) {
+       int temp = array[element1];
+       array[element1] = array[element2];
+       array[element2] = temp;
+    }
+
     public static void main(String[] args) {
        int[] minArray = {1,2,3,4,5,6,8,9,10,11};
+        bytPlats(minArray, 0,3);
         skriv_ut_array(minArray);
-        antal_över_8(minArray);
     }
 }
